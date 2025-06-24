58fac94469749886da09a350855456a495e5798a
huayun_zhouyonggui <zhouyg@hua-cloud.com.cn>
2025-06-24 18:59:34 +0800

    触发泳道

--- src/main/java/com/time/axis/WxCloudRunApplication.java
+++ src/main/java/com/time/axis/WxCloudRunApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WxCloudRunApplication {  
 
   public static void main(String[] args) {
+    // 泳道
     SpringApplication.run(WxCloudRunApplication.class, args);
   }
 }
